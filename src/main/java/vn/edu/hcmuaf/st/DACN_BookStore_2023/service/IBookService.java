20cbe08a4e80c7d879b26eac5c883a2f751155aa
huynhngocgiau <19130063@st.hcmuaf.edu.vn>
2023-06-13 20:17:21 +0700

    cart, thanh toan

--- src/main/java/vn/edu/hcmuaf/st/DACN_BookStore_2023/service/IBookService.java
+++ src/main/java/vn/edu/hcmuaf/st/DACN_BookStore_2023/service/IBookService.java
@@ -12,7 +12,19 @@ public interface IBookService {
     public List<BookDTO> findAll(Pageable pageable);
 
     public List<BookDTO> findAll();
+    public List<BookDTO> findHotBook(boolean isActive, boolean isHot);
 
+    public List<BookDTO> findNewBook(boolean isActive, boolean isNew);
+
+    public List<BookDTO> findAllHotBook(boolean isActive, boolean isHot, Pageable pageable);
+
+    public List<BookDTO> findAllNewBook(boolean isActive, boolean isNew, Pageable pageable);
+
+    public List<BookDTO> findByCategoryIdAnQuantityGreaterThan(int categoryId, int quantity);
+
+    public List<BookDTO> findByPriceBetween(int from, int to, Pageable pageable);
+
+    public List<BookDTO> findByPriceGreaterThan(int from, Pageable pageable);
 
     public List<BookDTO> findAllContainTitle(String title, Pageable pageable);
 
@@ -22,8 +34,13 @@ public interface IBookService {
     public int countByAuthorCode(String code);
 
     public int countAllByActive(boolean isActive);
+    public int countAllByHot(boolean isActive, boolean isHot);
 
+    public int countAllByNews(boolean isActive, boolean isNew);
     public int countAllByTitleContains(String titles);
+    public int countAllByPriceBetween(int from, int to);
+
+    public int countAllByPriceGreaterThan(int from);
 
     public List<String> autoCompleteTitle(String title);
 
@@ -32,4 +49,12 @@ public interface IBookService {
     public BookDTO findById(int id);
 
     public void deleteById(int id);
+    public List<String> autoCompleteTilte(String title);
+
+    public List<BookDTO> findAllByActiveAndDicount(boolean active, double discountFrom, double discountTo, Pageable pageable);
+
+    public int countAllByActiveAndDiscount(boolean active, double discountFrom, double discountTo);
+
+    public void updateQuantity(int quantity, int id);
+
 }
